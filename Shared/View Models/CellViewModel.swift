9174b50cf8731fe697be9387f6fd041973571990
Chad Parker <chad@chadparker.com>
2020-10-13 20:48:27 -0700

    Create groups, create Cell and CellViewModel

---
+++ Shared/View Models/CellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CellViewModel.swift
+//  GameOfLife
+//
+//  Created by Chad Parker on 10/13/20.
+//
+
+import Foundation
---
+++ Shared/Views/Cell.swift
@@ -0,0 +1,20 @@
+//
+//  Cell.swift
+//  GameOfLife
+//
+//  Created by Chad Parker on 10/13/20.
+//
+
+import SwiftUI
+
+struct Cell: View {
+    var body: some View {
+        Text("Yo!")
+    }
+}
+
+struct Cell_Previews: PreviewProvider {
+    static var previews: some View {
+        Cell()
+    }
+}
