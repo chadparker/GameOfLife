d196ddf2c4cb3df811c8fdab24bedcdb797a13f8
Chad Parker <chad@chadparker.com>
2020-10-13 19:21:43 -0700

    Create Xcode project

---
+++ Shared/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  Shared
+//
+//  Created by Chad Parker on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
