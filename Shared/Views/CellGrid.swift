5e1c5f417a54de67c87647ce3b24c0108a83edc1
Chad Parker <chad@chadparker.com>
2020-10-16 19:05:12 -0700

    CellGrid temporary try with a LazyVGrid
    But a HStack/VStack combo would work fine here

--- Shared/Views/CellGrid.swift
+++ Shared/Views/CellGrid.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct CellGrid: View {
+
+    let spacing: CGFloat = 2
+
+    @ObservedObject var gameModel = GameOfLifeModel()
+
+    let data = (1...25).map { "\($0)" }
+    var columns = [GridItem]()
+
+    init() {
+        columns = [GridItem(.fixed(30), spacing: spacing),
+                   GridItem(.fixed(30), spacing: spacing),
+                   GridItem(.fixed(30), spacing: spacing),
+                   GridItem(.fixed(30), spacing: spacing),
+                   GridItem(.fixed(30), spacing: spacing),]
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        LazyVGrid(columns: columns, spacing: spacing) {
+            ForEach(data, id: \.self) { item in
+                Cell(size: 30, alive: true)
+            }
+        }
     }
 }
+
+
+
 
 struct Grid_Previews: PreviewProvider {
     static var previews: some View {
