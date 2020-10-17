d26243f1851b068e38f4190d3c78aa35641ab88c
Chad Parker <chad@chadparker.com>
2020-10-16 19:03:44 -0700

    Basic Cell view fleshed out

--- Shared/Views/Cell.swift
+++ Shared/Views/Cell.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct Cell: View {
+
+    var size: CGFloat = 30
+    @State var alive: Bool = false
+
     var body: some View {
-        Text("Yo!")
+        ZStack {
+            Image(systemName: "xmark")
+                .font(Font.system(size: size/2.0, weight: .black))
+                .foregroundColor(Color("dead"))
+
+            Button(action: {
+                alive.toggle()
+            }, label: {
+                Square(cornerRadius: size/6)
+                    .foregroundColor(Color("alive"))
+                    .opacity(alive ? 1 : 0)
+                    .frame(width: size, height: size)
+            })
+        }
     }
 }
 
 struct Cell_Previews: PreviewProvider {
     static var previews: some View {
-        Cell()
+        VStack {
+            Cell(alive: false)
+            Cell(alive: true)
+        }
     }
 }
