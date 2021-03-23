db61cdbcba477f67752b67c9a3e558480708a279
Chad Parker <chad@chadparker.com>
2021-03-22 20:04:46 -0700

    Add InfoView

---
+++ Shared/Views/InfoView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoView.swift
+//  GameOfLife
+//
+//  Created by Chad Parker on 3/17/21.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
