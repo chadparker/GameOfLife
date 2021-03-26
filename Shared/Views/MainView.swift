9460a4b9ae1c521910bad6fac76740366d778ebc
Chad Parker <chad@chadparker.com>
2021-03-25 19:44:10 -0700

    Add InfoView to MainView in a ZStack, showing via @State `showingInfo`

--- Shared/Views/MainView.swift
+++ Shared/Views/MainView.swift
@@ -10,27 +10,32 @@ import SwiftUI
 struct MainView: View {
     
     @EnvironmentObject var gameModel: GameOfLifeModel
+    @State var showingInfo = true
     
     var body: some View {
-        GeometryReader { g in
-            VStack {
-                Image("header")
-                    .padding(.vertical, g.size.height/100)
-                HStack {
+        ZStack {
+            GeometryReader { g in
+                VStack {
+                    Image("header")
+                        .padding(.vertical, g.size.height/100)
+                    HStack {
+                        Spacer()
+                        Text("Generation")
+                            .foregroundColor(Color(.systemGray))
+                            .buttonText()
+                        Text("\(gameModel.generation)")
+                            .buttonText()
+                            .padding(.trailing, 10)
+                    }
+                    GridView(size: g.size.width)
+                    ConfigButtonsView()
+                    ButtonsView(showingInfo: $showingInfo)
                     Spacer()
-                    Text("Generation")
-                        .foregroundColor(Color(.systemGray))
-                        .buttonText()
-                    Text("\(gameModel.generation)")
-                        .buttonText()
-                        .padding(.trailing, 10)
                 }
-                GridView(size: g.size.width)
-                ConfigButtonsView()
-                ButtonsView()
-                Spacer()
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
+            InfoView()
+                .opacity(showingInfo ? 1 : 0)
         }
     }
 }
