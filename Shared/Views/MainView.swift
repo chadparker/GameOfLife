f92646ef9871476e973804d7f5ab52fac9b63ff4
Chad Parker <chad@chadparker.com>
2021-03-27 11:41:22 -0700

    Info view hidden on launch

--- Shared/Views/MainView.swift
+++ Shared/Views/MainView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainView: View {
     
     @EnvironmentObject var gameModel: GameOfLifeModel
-    @State var showingInfo = true
+    @State var showingInfo = false
     
     var body: some View {
         ZStack {
